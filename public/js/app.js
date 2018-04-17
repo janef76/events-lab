b5762ea4d601d99c7dd9192f4145acaadc25603f
janef76 <janeflucker@btinternet.com>
2018-04-17 12:21:43 +0100

    submit form works

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,40 @@
+const handleFormSubmit = function(event) {
+  event.preventDefault();
+  const title = this.title.value;
+  const author = this.author.value;
+  const category = this.category.value;
+  const genre = this.genre.value;
+  addBookToList(title, author, category, genre);
+  console.log(title, author, category, genre);
+
+
+}
+
+const addBookToList = function (titleInput, authorInput, categoryInput, genreInput) {
+
+  const books = document.querySelector('#book-list')
+  const book = document.createElement('ul');
+
+  const titleLi = document.createElement('li');
+  const authorLi = document.createElement('li');
+  const categoryLi = document.createElement('li');
+  const genreLi = document.createElement('li');
+
+  titleLi.textContent = titleInput;
+  authorLi.textContent = authorInput;
+  categoryLi.textContent = categoryInput;
+  genreLi.textContent = genreInput;
+
+
+  books.appendChild(book);
+  book.appendChild(titleLi);
+  book.appendChild(authorLi);
+  book.appendChild(categoryLi);
+  book.appendChild(genreLi);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
+  const save = document.querySelector('#book-form');
+  save.addEventListener('submit', handleFormSubmit);
 });
